22330a7da006892807a6b46eb20a4d6054351ee4
goalart09 <294795188@qq.com>
2021-09-10 09:21:24 +0800

    hahahaha1

--- settings.py
+++ settings.py
@@ -19,7 +19,7 @@ class Settings():
      self.bullets_allowed = 3
 
      #外星人设置
-     self.fleet_drop_speed =10
+     self.fleet_drop_speed =9
 
      # 以什么样的速度加快游戏节奏
      self.speedup_scale = 1.1
